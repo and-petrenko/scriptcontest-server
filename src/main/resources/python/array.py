22e87b4c5f28ff45b5e415262bd7b23c8e869bf3
DrexonV2 <40270985+DrexonV2@users.noreply.github.com>
2021-05-17 18:19:38 +0300

    Version_1
    Добавлена правильная генерация кораблей

--- src/main/resources/python/array.py
+++ src/main/resources/python/array.py
@@ -1,13 +1,67 @@
+import random
+def build(arr,l):
+    a = random.randint(0,1)
+    Testt = True
+    test = True
+    if a == 0:
+        while Testt:
+            test = True
+            x = random.randint(1,11-l)
+            y = random.randint(1,10)
+            for i in range(l):
+                if arr[x + i][y] != 0:
+                    test = False
+                    break
+            if test == True:
+                for i in range(3):
+                    for j in range(l+2):
+                        arr[x + j - 1][y + i - 1] = 2
+                for i in range(l):
+                    arr[x+i][y] = 1
+                Testt = False
+    else:
+        while Testt:
+            test = True
+            x = random.randint(1,10)
+            y = random.randint(1,11-l)
+            for i in range(l):
+                if arr[x][y + i] != 0:
+                    test = False
+                    break
+            if test == True:
+                for i in range(l+2):
+                    for j in range(3):
+                        arr[x + j - 1][y + i - 1] = 2
+                for i in range(l):
+                    arr[x][y+i] = 1
+                Testt = False
+
 def getShipsPosition():
-    arr = []
+    array = []
+    array12 = []
+    for i in range(12):
+        array12.append([])
+        for j in range(12):
+            array12[i].append(0)
     for i in range(10):
-        arr.append([])
+        array.append([])
         for j in range(10):
-            arr[i].append(0)
-    arr[1][2] = 1
-    arr[5][7] = 3
-    arr[2][6] = 6
-    arr[8][2] = 2
-    return arr
+            array[i].append(0)
+    build(array12, 4)
+    for i in range(2):
+        build(array12, 3)
+    for i in range(3):
+        build(array12, 2)
+    for i in range(4):
+        build(array12, 1)
+    for i in range(1,11):
+        for j in range(1,11):
+            if array12[i][j] == 1:
+                array[i-1][j-1] = 1
+    for n in array:
+        print(n)
+    return array
+
+getShipsPosition()
 
 
